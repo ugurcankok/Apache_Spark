d0b8df149881da5a49ba9f6d5c1b39c8b58d3f9a
ugurcankok <ugurcankok0@gmail.com>
2023-11-24 22:35:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Apache Kafka Repository
+
+You can look at my work on Apache Spark in this repository.
+
+In this repository, you will find my works in ***src*** folder:
+
+* [PySpark]()
---
+++ src/Spark Demo/Fundamentals/pySpark-fundamentals.py
@@ -0,0 +1,44 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("Fundamentals").getOrCreate()
+
+file_path = "/src/Spark Demo/files/test1.csv"
+
+#df_spark = spark.read.csv(file_path, header = True)
+
+df_spark = spark.read.option("header", "true").csv(file_path)
+
+df_spark.show()
+
+type(df_spark)
+
+df_spark.printSchema()
+
+# Data preprocessing
+
+df_spark.columns
+
+df_spark.head(4) # get result as a list format
+
+df_spark.select('Name') # get a column 'Name', slicing is not working for spark dataframe
+
+df_spark.select(['Name', 'Experience'])
+
+df_spark.dtypes
+
+df_spark.describe()
+
+df_spark.withColumn('New Experience Column', df_spark['Experience']) # adding columns
+
+df_spark.drop('New Experience Column') # drop columns
+
+df_spark.withColumnRenamed('Name', 'New Name') # rename the columns
+
+
+
+
+
+
+
+
+
